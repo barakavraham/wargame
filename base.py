dfda7d30f412afb9cb485166c3eab128c862e30d
Daniel Attiach <daniel@at-bay.com>
2019-04-26 15:40:24 +0300

    fixed linting

--- base.py
+++ base.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '827677ccbc5d49e567a7fceed8b43c2682c8469c2aa2b84d648349ef2fd7f4d0'
+app.config['SECRET_KEY'] = '827677ccbc5d49e567a7fceed8b43c26v4uh4hwp4gihr'
+
 
 @app.route("/")
 def base():
     return render_template('base.html')
 
+
 @app.route("/shop")
 def shop():
     return render_template('shop.html')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -20,10 +23,13 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
+
 @app.route("/login")
 def login():
+    # test test test test test test test test test test test test test
     form = LoginForm()
     return render_template('login.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -4,20 +4,21 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', 
-                        validators=[DataRequired(), Length(min = 3, max = 10)])
+    username = StringField('Username',
+                           validators=[DataRequired(),Length(min=3, max=10)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password',
-                        validators=[DataRequired(), Length(min = 6, max = 15)])
-    confirm_password = PasswordField('Confirm Paswword',
-                        validators=[DataRequired(), EqualTo('password')])
+                             validators=[DataRequired(), Length(min = 6, max = 15)])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email])
     password = PasswordField('Password',
-                        validators=[DataRequired(), Length(min = 6, max = 15)])
+                             validators=[DataRequired(), Length(min = 6, max = 15)])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
