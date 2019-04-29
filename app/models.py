eed4d1250a2aaeb863ec4812da2eca0905f30a40
Daniel Attiach <daniel@at-bay.com>
2019-04-29 14:15:39 +0300

    changes

--- app/models.py
+++ app/models.py
@@ -1,5 +1,10 @@
-from app import db
+from app import db, login_manager
 from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 class User(db.Model, UserMixin):
