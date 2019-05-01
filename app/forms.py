e6ced8c3b64a080c34301cdeaa9296f0c967282a
Barak Avraham <warkbarak@gmail.com>
2019-05-01 15:45:15 +0300

    login fix

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from app.models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app import bcrypt
 
 
 class RegistrationForm(FlaskForm):
@@ -27,6 +28,11 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('This email is already taken')
 
+    def validate_army_name(self, army_name):
+        for letter in army_name.data:
+            if not (letter >= 'a' and letter <= 'z') or (letter >= 'A' and letter <= 'Z'):
+                raise ValidationError('army name must contain english letters')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
@@ -35,3 +41,14 @@ class LoginForm(FlaskForm):
                         validators=[DataRequired(), Length(min = 6, max = 32)])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+    def validate_email(self, email):
+        input_check = User.query.filter_by(email=email.data).first()
+        if not input_check:
+            raise ValidationError("Invaild email or password")
+        elif not bcrypt.check_password_hash(input_check.password, self.password.data):
+            raise ValidationError("Invaild email or password")
+
+
+
+
