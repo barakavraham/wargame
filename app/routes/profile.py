bdb90a7673e69127b419ba029c18e3cbc862a8a4
Barak Avraham <warkbarak@gmail.com>
2019-05-12 18:11:23 +0300

    added shop functionality

---
+++ app/routes/profile.py
@@ -0,0 +1,15 @@
+from flask import render_template, Blueprint
+from flask_login import login_required
+from app.models import Army
+
+
+profile = Blueprint('profile', __name__, template_folder='templates')
+
+
+@profile.route("/<army_name>")
+@login_required
+def index(army_name):
+    Army.query.filter_by(army_name=army_name).first_or_404()
+    field = Army.query.filter_by(army_name=army_name).first().field
+    clan = Army.query.filter_by(army_name=army_name).first().clan
+    return render_template('profile.html', army_name=army_name, field=field, clan=clan)
